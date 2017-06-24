66667257a207fa9a64a0699f06b5fc71c3770802
Luciano Ramalho <luciano@ramalho.org>
2017-06-24 15:04:28 -0300

    compareitor tabajara

--- compareitor.py
+++ compareitor.py
@@ -1,6 +1,6 @@
 from random import randrange, choice
 
-TURNOS = 10
+TURNOS = 6
 OPERADORES = ['==', '!=', '<', '>', '<=', '>=']
 
 print('%' * 20, 'COMPAREITOR TABAJARA', '%' * 20)
@@ -11,21 +11,29 @@ for i in range(TURNOS):
     a = randrange(10)
     b = randrange(10)
     op = choice(OPERADORES)
-    expressao = '%s %s %s' % (a, op, b)
+    expressao = str(a) + ' ' + op + ' ' + str(b)
     print('_' * 50, 'questão', i+1)
     print('a =', a)
     print('b =', b)
-    pergunta = 'a {} b\t\t(s/n)? '.format(op)
+    pergunta = 'a ' + op + ' b\t\t(s/n)? '
     resposta = input(pergunta)
     resposta = resposta == 's'
     if eval(expressao) == resposta:
         print('\t\t\tcerto!')
-        acertos += 1
+        acertos = acertos + 1
     else:
         print('\t\t\terrado...')
 
 print('%' * 62)
 porcentagem = acertos / TURNOS * 100
-mensagem = 'acertos (%.1f%%)' % porcentagem
+mensagem = 'acertos (%.1f)' % porcentagem + '%'
 print('RESULTADO:\t', acertos, mensagem)
 print('%' * 62)
+if porcentagem == 100:
+    print('Parabéns, você acertou todas!!!')
+elif porcentagem > 50:
+    print('Acertou mais da metade, continue praticando.')
+elif porcentagem == 0:
+    print('Você sabe tudo, mas tá de sacanagem.')
+else:
+    print('Você precisa praticar mais.')
